cedc568ed9b201e46c6dd933e3eb5e5603c9b739
Desree <desree@tshimologong.joburg>
2020-06-24 12:49:50 +0200

    MyNutrionFiles

---
+++ script.js
@@ -0,0 +1,14 @@
+function myFunction()
+{
+    let x = document.getElementById("Menu");
+
+    if (x.style.display === "block")
+    {
+        x.style.display = "none";
+    }
+
+    else
+    {
+    x.style.display = "block";
+}
+}
